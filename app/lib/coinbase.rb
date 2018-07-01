9237ea596355a9bb3726f0b58ed4a8f8f4ed8547
Jiří Čech <jiri.cech@juicymo.cz>
2018-07-01 23:53:02 +0200

    Add coinbase api class

---
+++ app/lib/coinbase.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Coinbase
+  include HTTParty
+  format :json
+  base_uri 'https://api.pro.coinbase.com'
+
+  def initialize
+    @options = {
+      headers: {
+        'CB-ACCESS-KEY' => ENV['CB_ACCESS_KEY'],
+        'CB-ACCESS-PASSPHRASE' => ENV['CB_ACCESS_PASSPHRASE'],
+        'Content-Type' => 'application/json',
+        'User-Agent' => 'Ruby on Rails'
+      }
+    }
+  end
+
+  def accounts
+    timestamp = time.to_s
+
+    @options[:headers].merge!(
+      'CB-ACCESS-SIGN' => signature('/accounts', '', timestamp),
+      'CB-ACCESS-TIMESTAMP' => timestamp
+    )
+    response = self.class.get('/accounts', @options)
+    ap response.parsed_response
+  end
+
+  def time
+    self.class.get('/time', @options).parsed_response['epoch']
+  end
+end
+
+private
+
+def signature(request_path = '', body = '', timestamp = nil, method = 'GET')
+  body = body.to_json if body.is_a?(Hash)
+  timestamp ||= Time.now.to_i
+
+  what = "#{timestamp}#{method}#{request_path}#{body}"
+
+  secret = Base64.decode64(ENV['CB_ACCESS_SECRET'])
+  hash = OpenSSL::HMAC.digest('sha256', secret, what)
+  Base64.strict_encode64(hash)
+end
