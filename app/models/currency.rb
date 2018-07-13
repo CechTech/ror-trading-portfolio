59b224b5e8566ec9252c62d476fc0a57455dedc6
Jiří Čech <jiri.cech@juicymo.cz>
2018-07-13 11:55:24 +0200

    Add basic currency model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Currency < ApplicationRecord
+  validates :title, presence: true
+  validates :symbol, presence: true
+
+  auto_strip_attributes :description, squish: true
 end
