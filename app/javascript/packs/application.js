711111aec1480a0b620bee310aef5ccb94a8b0eb
Jiří Čech <developer@jiricech.com>
2018-09-08 14:11:29 +0200

    Add stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,11 @@
 import 'stylesheets/application'
 
-console.log('Hello World from Webpacker')
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+// The path you may like to change to under `pack` that path will be `./controllers`
+// but convention will be in `/app/javascript/controllers`
+const context = require.context("controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
