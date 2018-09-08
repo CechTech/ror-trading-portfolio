8719e63fbbb005bf91abfddbd6d77586522bbd87
Jiří Čech <developer@jiricech.com>
2018-09-08 14:26:05 +0200

    Add tags to application slim file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,5 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
-// The path you may like to change to under `pack` that path will be `./controllers`
-// but convention will be in `/app/javascript/controllers`
 const context = require.context("controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
-
