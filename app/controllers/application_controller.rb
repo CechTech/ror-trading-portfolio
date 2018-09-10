949ba5284b292a645b58c2777682f10bb12485bf
Jiří Čech <developer@jiricech.com>
2018-09-10 15:54:28 +0200

    Authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
   prepend_view_path Rails.root.join('frontend')
 end
