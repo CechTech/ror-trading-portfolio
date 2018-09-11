fad60a606f2cb80b7f14257af56a572163864d68
Jiří Čech <developer@jiricech.com>
2018-09-11 20:32:23 +0200

    Add line chart and improve behavior

--- frontend/controllers/charts_controller.js
+++ frontend/controllers/charts_controller.js
@@ -4,6 +4,7 @@ import Chart from "chart.js";
 export default class extends Controller {
   connect() {
     this.fetch();
+    this.line();
   }
 
   fetch() {
@@ -31,6 +32,33 @@ export default class extends Controller {
               "rgba(153, 102, 255, 1)",
               "rgba(255, 159, 64, 1)"
             ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+
+  line() {
+    return new Chart($("#myLineChart"), {
+      type: "line",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
             borderWidth: 1
           }
         ]
